0865f8321a25d6f60cf82fe4b00c02172d2e56b3
Allison Palmer <60831298+alpa3473@users.noreply.github.com>
2021-07-16 14:51:22 -0600

    Pick up tile (#10)
    * added tile select component; fixed spacing for tile floor
    
    * change tile select to use world positions
    
    * added select/throw tile for player
    
    Co-authored-by: Peter Gyory <ptgyory@gmail.com>

--- src/components/Shooting.js
+++ src/components/Shooting.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent('shooting', {
     schema: {
-      velocity: {type: 'int', default: 15}
+      velocity: {type: 'int', default: 50}
     },
     tick: function () {
       // checks for when the ball is below y= -100, then returns it to the pool
@@ -14,11 +14,13 @@ AFRAME.registerComponent('shooting', {
     },
     shoot: function () {
       // this avatar will eventually be the player character
-      let avatar = document.querySelector('#boxThing').object3D
+      // this.el.setAttribute()
+      let avatar = this.el.object3D
       let avatarDir = avatar.getWorldDirection()
       currentPosition = this.el.getAttribute('position')
+      // console.log(currentPosition)
       el = this.el
-      let position = [currentPosition.x,currentPosition.y, currentPosition.z ]
+      let position = [avatarDir.x,avatarDir.y+25, avatarDir.z ]
       let unitVecDiv = Math.sqrt(Math.pow(currentPosition.x, 2)+ Math.pow(currentPosition.y, 2) + Math.pow(currentPosition.z, 2))
       // finds the correct length of each velocity component so that each throw has the same total velocity
       for (let x = 0; x < 3; x++) {
@@ -26,31 +28,9 @@ AFRAME.registerComponent('shooting', {
       }
       // applies the impulse the body (waiting for load isn't necessary since we are calling on pool components which are already loaded)
       el.body.applyImpulse(
-        /* impulse */        new CANNON.Vec3(avatarDir.x, avatarDir.y, avatarDir.z ),
+        /* impulse */        new CANNON.Vec3(position[0], position[1], position[2] ),
                             //  new CANNON.Vec3(1, 1, 1 ),
         /* world position */ new CANNON.Vec3().copy(el.getAttribute('position'))
         );
     }
   });
-AFRAME.registerComponent('shooter', {
-  play: function() {
-  // listens for spacebar to be pressed
-  let player = this.el
-  window.addEventListener("keydown", function(e){
-    let sceneEl =document.querySelector('#ball-pool');
-      if(e.keyCode === 32) { // spacebar
-        // takes the entity from the pool
-        let el = sceneEl.components.pool__projectile.requestEntity();
-        let playerPov = player
-        let playerPos = playerPov.getAttribute('position')
-        let currentPos = sceneEl.getAttribute('position')
-        // sets the ball position and rotation to that of the character
-        el.setAttribute('position', (-currentPos.x+playerPos.x)+" "+(-currentPos.y+playerPos.y) +" "+ (-currentPos.z + playerPos.z))
-        el.setAttribute('rotation', playerPov.getAttribute("rotation"))
-        let elComp = el.components.shooting
-        el.play()
-        // elComp.shoot()
-      }
-  });
-  }
-});
